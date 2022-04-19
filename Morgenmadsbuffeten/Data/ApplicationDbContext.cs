814992f3051b97169a563f4f22b50ab2e9078be9
Anne Sophie Michelsen <201205720@post.au.dk>
2022-04-19 14:28:46 +0200

    FluentAPI added

--- Morgenmadsbuffeten/Data/ApplicationDbContext.cs
+++ Morgenmadsbuffeten/Data/ApplicationDbContext.cs
@@ -1,13 +1,29 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Morgenmadsbuffeten.Models;
 
 namespace Morgenmadsbuffeten.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        DbSet<RoomBooking> RoomBookings { get; set; }
+        DbSet<BreakfastBooking> BreakfastBookings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<RoomBooking>()
+                .HasKey(rb => rb.RoomNumber);
+
+            builder.Entity<BreakfastBooking>()
+                .HasOne(bb => bb.RoomBooking)
+                .WithMany(rb => rb.BreakfastBookings);
         }
     }
 }
