6e600ad55818bf2b6516d1df95ee0e25dc2e6bae
Jacob Gade <jacobcgade@gmail.com>
2022-04-20 11:15:47 +0200

    Create new link updated

--- Morgenmadsbuffeten/DTOs/RoomsBreakfastDTO.cs
+++ Morgenmadsbuffeten/DTOs/RoomsBreakfastDTO.cs
@@ -2,7 +2,6 @@
 {
     public class RoomsBreakfastDTO
     {
-        public DateTime Date { get; set; }
         public long RoomNumber { get; set; }
         public int NumberOfChildren { get; set; }
         public int NumberOfAdults { get; set; }
