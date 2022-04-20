67fe56fdbb2284d68ba6823718a6c1ea632fc31a
Jacob Gade <jacobcgade@gmail.com>
2022-04-20 12:47:42 +0200

    New Room Booking Scaffolding

---
+++ Morgenmadsbuffeten/Pages/NewRoomBookingView/Edit.cshtml.cs
@@ -0,0 +1,78 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Morgenmadsbuffeten.Data;
+using Morgenmadsbuffeten.Models;
+
+namespace Morgenmadsbuffeten.Pages.NewRoomBookingView
+{
+    public class EditModel : PageModel
+    {
+        private readonly Morgenmadsbuffeten.Data.ApplicationDbContext _context;
+
+        public EditModel(Morgenmadsbuffeten.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public RoomBooking RoomBooking { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            RoomBooking = await _context.RoomBookings.FirstOrDefaultAsync(m => m.RoomNumber == id);
+
+            if (RoomBooking == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(RoomBooking).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RoomBookingExists(RoomBooking.RoomNumber))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool RoomBookingExists(long id)
+        {
+            return _context.RoomBookings.Any(e => e.RoomNumber == id);
+        }
+    }
+}
