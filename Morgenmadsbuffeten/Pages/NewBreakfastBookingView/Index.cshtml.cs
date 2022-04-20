a1f011283f6487febcb93dde1539f5b6b1d3b16a
Anne Sophie Michelsen <201205720@post.au.dk>
2022-04-20 12:53:58 +0200

    NewBreakfastBooking Index added

--- Morgenmadsbuffeten/Pages/NewBreakfastBookingView/Index.cshtml.cs
+++ Morgenmadsbuffeten/Pages/NewBreakfastBookingView/Index.cshtml.cs
@@ -3,14 +3,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Morgenmadsbuffeten.Data;
+using Morgenmadsbuffeten.DTOs;
 using Morgenmadsbuffeten.Models;
 
 namespace Morgenmadsbuffeten.Pages.NewBreakfastBookingView
 {
+    [Authorize("CanSeeReceptionStuff")]
     public class IndexModel : PageModel
     {
         private readonly Morgenmadsbuffeten.Data.ApplicationDbContext _context;
@@ -20,12 +23,18 @@ namespace Morgenmadsbuffeten.Pages.NewBreakfastBookingView
             _context = context;
         }
 
-        public IList<BreakfastBooking> BreakfastBooking { get;set; }
+        public IList<RoomsBreakfastDTO> BreakfastBookings { get;set; }
 
         public async Task OnGetAsync()
         {
-            BreakfastBooking = await _context.BreakfastBookings
-                .Include(b => b.RoomBooking).ToListAsync();
+            BreakfastBookings = await _context.BreakfastBookings
+                 .Where(bb => (bb.AdultsCheckedIn > 0 || bb.ChildrenCheckedIn > 0) && bb.Date == DateTime.Today)
+                .Select(bb => new RoomsBreakfastDTO
+                {
+                    RoomNumber = bb.RoomBooking.RoomNumber,
+                    NumberOfChildren = bb.ChildrenCheckedIn,
+                    NumberOfAdults = bb.AdultsCheckedIn,
+                }).ToListAsync();
         }
     }
 }
